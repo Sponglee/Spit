13769c6256ff2d012c918da97e38c83c16e87728
SolidHinken <solidhinken@gmail.com>
2020-02-16 23:06:08 +0300

    Initial commit

---
+++ Assets/CameraData.cs
@@ -0,0 +1,12 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraData : MonoBehaviour
+{
+    public CinemachineVirtualCamera targetCam;
+    public string targetName;
+
+}
+
